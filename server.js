8c608e4b18646728edd25600c34ad6f0a00f02aa
anisurrkhan <anisurk24@gmail.com>
2020-09-25 03:47:52 -0700

    repo is running combined

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const passport = require("passport");
-// const http = require("http");
 
 // const path = require("path");
 const users = require("./routes/api/users");
-// const posts = require("./routes/api/posts");
 const cars = require("./routes/api/cars");
 const images = require("./routes/api/images");
 const search = require("./routes/api/search");
@@ -33,15 +31,13 @@ app.use("/api/cars", cars);
 app.use("/api/images", images)
 app.use("/api/search", search)
 
-const port = process.env.PORT || 3080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("repo-coin/build"));
-//   app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "repo-coin", "build", "index.html"));
-//   });
-} else {
-    app.use(express.static("repo-coin/public"));
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
